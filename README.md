074199ab8568f1ed5a234e0885f69aec369c5449
dishitabehera <70992046+dishitabehera@users.noreply.github.com>
2020-12-26 11:03:06 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# p5.play-boilerplate
-Boiler plate for p5.play
+# angryBirdsStage2.5
+Angry Birds stage 2.5 with Class Inheritance and Images
--- sketch.js
+++ sketch.js
@@ -1,57 +1,136 @@
-const Engine = Matter.Engine
-const World = Matter.World
-const Bodies = Matter.Bodies
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
-var box1, box2, pig1, ground1;
+var box1, pig1;
+var backgroundImg,platform;
+var gamestate = "START"
 
-function preload(){
-  backgroundIMG=loadImage("sprites/bg.png");
-}
-function setup() {
-  createCanvas(800,400);
-  engine = Engine.create();
-  
-  world = engine.world;
-  
-  box1 = new Box(450,310,70,70);
-  box2 = new Box(270,310,70,70);
-  pig1 = new Pig(350, 310);
-  log1 = new Log(350, 270,250,PI)
+var score = 0
+function preload() {
+    backgroundImg1 = loadImage("sprites/bg.png");
+    getBgImage()
 
-  box3 = new Box(450,250, 70, 70);
-  box4 = new Box(270, 250, 70, 70);
-  pig2 = new Pig(350, 250);
-  log2 = new Log(350, 220, 250, PI);
-  ground1 = new Ground(width/2, height, width);
-  
-  pig3 = new Pig(350, 200);
-
-  log3 = new Log(320, 150, 100, -PI/5);
-  log4 = new Log(380, 150, 100, PI/5);
-
-  bird1 = new Bird(20, 20);
 }
 
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+	//creating ground
 
-function draw() {
-  background(backgroundIMG);  
-  Engine.update(engine);
-  
-  box1.display()
-  box2.display();
-  pig1.display();
-  log1.display();
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
-  box3.display()
-  box4.display()
-  pig2.display()
-  log2.display()
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
 
-  pig3.display();
-  log3.display();
-  log4.display();
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
 
-  bird1.display();
-  ground1.display();
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,40);
+    
+    chain1 = new Slingshot(bird.body, {x:200,y:40});
+   
+ 
 }
+
+function draw(){
+    if(backgroundImg){
+    background(backgroundImg);
+    }else{
+        background(backgroundImg1)
+    }
+    Engine.update(engine);
+    push();
+    strokeWeight(5)
+    textSize(20)
+    text( "score: "+ score, 1000, 20)
+    pop();
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    chain1.display();
+    pig1.score();
+    pig3.score();
+    
+
+
+}
+function keyPressed(){
+    if(keyCode === 32){
+        chain1.attach(bird.body)
+        Matter.Body.setPosition( bird.body,{x: 200, y: 40});
+        gamestate = "START"
+    }
+}
+
+function mouseDragged(){
+    if(gamestate === "START")
+    Matter.Body.setPosition( bird.body,{x: mouseX, y: mouseY});
+
+}
+
+function mouseReleased(){
+    chain1.fly();
+    gamestate = "FLY"
+}
+
+async function getBgImage(){
+    var response = await fetch("https://worldclockapi.com/api/json/est/now")
+    responseJSON = await response.json()
+    var time = responseJSON.currentDateTime
+    var hours = time.slice(11,13)
+    if(hours > 6 && hours < 19){
+        bg = "sprites/bg.png"
+
+    }else{
+        bg = "sprites/bg2.png"
+    }
+    backgroundImg = loadImage(bg)
+}
+
+/*JSON -Javascript Object Notation
+var x = {
+    name1: value,
+    name2:value,
+    name3:value
+}
+
+var options = {
+    restitution:1,
+    friction: 0.5,
+    density:1
+}
+options.friction 
+options.density
+*/
+
+/*API- Application program Interface
+
+*/
